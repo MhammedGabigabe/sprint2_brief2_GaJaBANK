4a5993698c787f7f24392c7bcbb0a995d7eebfc6
MEDJAJ <jaj53290@gmail.com>
2025-11-13 16:45:22 +0100

    feat : realisation de page js de recharge

---
+++ GaJa_BANK/payement.js
@@ -0,0 +1,6 @@
+const facteure=document.getElementById("fact");
+const fournisseure=document.getElementById("four");
+const ref=document.getElementById("ref");
+const montant=document.getElementById("montant");
+
+
