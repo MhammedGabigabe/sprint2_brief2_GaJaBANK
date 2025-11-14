b51e16551cb63d758a3199efaf2240bda8612f44
Gabigabe <mhammed.gabigabe@gmail.com>
2025-11-14 23:29:27 +0100

    refactor: derniere amelioration

--- GaJa_BANK/footer.js
+++ GaJa_BANK/footer.js
@@ -2,13 +2,22 @@ const menuHeaders = document.querySelectorAll(".menu-header");
 
   menuHeaders.forEach(header => {
     header.addEventListener("click", () => {
-      const submenu = header.nextElementSibling; // le div juste après (le sous-menu)
+      const submenu = header.nextElementSibling; 
       const icon = header.querySelector(".fa-angle-up");
 
-      // Toggle affichage du sous-menu
       submenu.classList.toggle("hidden");
 
-      // Rotation de la flèche
       icon.classList.toggle("rotate-180");
     });
   });
+
+  function closeNav(){
+ footer.classList.remove("w-[320px]");
+ footer.classList.add("w-[0px]");
+ }
+
+ function openNav(){
+    
+ footer.classList.remove("w-[0px]");
+ footer.classList.add("w-[320px]");
+ }
